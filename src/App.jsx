3add923c85eb47910fcae61608f521d014c8d635
Abdul-Rehman345900 <144555504+Abdul-Rehman345900@users.noreply.github.com>
2023-10-01 17:09:29 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,77 @@
+import './App.css'
+
 function App(){
   return(
-    <h1>Hello World</h1>
+    <div className='container'>
+
+    
+    <div className="header" >
+        <img src="img.jpeg" alt=" "></img>  
+    
+        <div className="name">Abdul-Rehman</div>
+        <div className="job-title"><b>Web Developer</b> </div>
+        </div>
+    <div className="part_1">
+    <div className="sec_1" >
+    <div className="contact-info" >
+        <h3 className='same'>Contact Info</h3>
+        <p>Email: rehman.niazlhr@gmail.com</p>
+        <p>Phone: 0322-985-8397</p>
+        </div>
+
+    <div className="section">
+        <div className="section-title">Education</div>
+        <div className="section-content">
+            <p><strong>Collage Name: D.D.Collage lahore</strong></p>
+            <p>I am compeleted my Inter in Computer Science</p>
+            <p>PassOut: May 2022</p>
+        </div>
+        </div>
+        </div>
+        <div className="section">
+        <div className="section-title">Summary</div>
+        <div className="section-content">
+            <p className='para1'>I
+            "As a beginner web developer, I am passionate about creating innovative and user-friendly websites. I have a solid foundation in HTML, CSS, and JavaScript, which allows me to build responsive and visually appealing web applications. I am eager to learn and stay up-to-date with the latest web development technologies and best practices i am doning this great experiece from (NEXUS BERRY) platfom .
+
+            </p>
+        </div>
+    </div>
+
+    </div>
+   
+   <div className="exp-skill_sec">
+   <div className="section">
+        <div className="section-title">Experience</div>
+        <div className="section-content">
+            <p><strong>I am beginner</strong></p>
+            <p>Web Developement from Nexus Berry Institute</p>
+        </div>
+    </div>
+    <div className="section">
+        <div className="section-title">Skills</div>
+        <div className="section-content">
+            <p>HTML, CSS, JavaScript, React, Node.js</p>
+            <p>Version Control (Git), Responsive Web Design</p>
+        </div>
+    </div>
+   </div>
+  
+   <footer>
+    <div>
+        <img src="./icons/mail-outline.svg" alt="" />
+        <img src="./icons/logo-instagram.svg" alt="" />
+        <img src="./icons/logo-facebook.svg" alt="" />
+        
+    </div>
+
+   </footer>
+
+    </div>
+
   )
 }
+
+    
+
 export default App;
